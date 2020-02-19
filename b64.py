972589068a0f0f19310ba5be110f3c5532bd8ed9
Jason Mar <jasonmar@google.com>
2020-02-19 17:16:47 -0600

    get AUDIENCE from env

--- b64.py
+++ b64.py
@@ -3,6 +3,7 @@ import base64
 import fileinput
 import google.auth.transport.requests
 from google.oauth2 import id_token
+import os
 
 # Use this script to verify a token on the client
 # echo "$token" | ./b64.py
@@ -12,7 +13,7 @@ def run():
     for line in fileinput.input():
         buf.append(line.rstrip('\n'))
     buf.append("===")
-    audience = "https://secureinit.local"
+    audience = os.environ['AUDIENCE']
     token = ''.join(buf)
     request = google.auth.transport.requests.Request()
     payload = id_token.verify_token(token, request=request, audience=audience)
